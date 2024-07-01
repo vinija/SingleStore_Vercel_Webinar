644046eb7767df1f55d99cabe403ac27c20f3ad7
VinijaJain <vinija@gmail.com>
2024-06-30 21:02:41 -0700

    add

--- pages/api/initialize.ts
+++ pages/api/initialize.ts
@@ -2,6 +2,15 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { createTable } from '@/lib/singlestore';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
